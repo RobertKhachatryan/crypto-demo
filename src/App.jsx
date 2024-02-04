00f766b1de0243baf20a45373cbbaa4a19ff9a0b
Robert <robert.khachatryan2003@gmail.com>
2024-02-05 03:05:36 +0400

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { CryptoContextProvider } from "./context/crypto-context";
+import { AppLayout } from "./components/Layout/AppLayout";
+
+export default function App() {
+  return (
+    <CryptoContextProvider>
+      <AppLayout />
+    </CryptoContextProvider>
+  );
+}
